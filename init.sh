aafc50c228bc8e5eb517f1d21afa493e578b61b7
MaxOvcharov <ma12xxx@rambler.ru>
2017-02-09 13:00:21 +0300

    Added new comman for init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-sudo rm -rf /etc/nginx/sites-enabled/
-sudo ln -sf nginx.conf  /etc/nginx/sites-enabled/default
+sudo rm -rf /etc/nginx/sites-enabled/*
+sudo chmod 777 nginx.conf
+sudo cp nginx.conf /etc/nginx/sites-enabled/ginx.conf
 sudo nginx -t
 sudo /etc/init.d/nginx restart
