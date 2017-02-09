9d7b194d9e852c1f737826a2084187fe15913e1f
MaxOvcharov <ma12xxx@rambler.ru>
2017-02-09 17:18:20 +0300

    Create a systemd Unit File

--- init.sh
+++ init.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+# NGINX settings
 sudo rm -rf /etc/nginx/sites-enabled/*
 sudo chmod 777 nginx.conf
 sudo cp nginx.conf /etc/nginx/sites-enabled/nginx.conf
 sudo nginx -t
 sudo /etc/init.d/nginx restart
 
+# Supervisor settings
+sudo unlink /var/run/supervisor.sock
 sudo cp gunicorn.conf /etc/supervisor/conf.d/
 sudo supervisord -c /etc/supervisor/supervisord.conf
+
 
