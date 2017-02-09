fba1cdff902370eaafb9b1cf00d51c1d5a5feac6
MaxOvcharov <ma12xxx@rambler.ru>
2017-02-09 14:28:47 +0300

    Bug fixed

--- init.sh
+++ init.sh
@@ -5,7 +5,6 @@ sudo chmod 777 nginx.conf
 sudo cp nginx.conf /etc/nginx/sites-enabled/ginx.conf
 sudo nginx -t
 sudo /etc/init.d/nginx restart
-sudo cp gunicorn.conf /etc/supervisor/conf.d/ViberBot.conf
 sudo supervisorctl reread
 sudo supervisorctl update
-sudo supervisorctl start ViberBot
+
