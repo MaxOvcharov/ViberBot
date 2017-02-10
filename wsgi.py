a42a0d7f6805ec63461453b20191b1049790f3e3
MaxOvcharov <ovcharovmax@yandex.ru>
2017-02-10 15:13:54 +0300

    Try to understand what's wronggit push!

--- wsgi.py
+++ wsgi.py
@@ -2,11 +2,24 @@ from simpleViberBot import app, set_webhook
 import time
 import sched
 import threading
+import logging
+import os
+
+logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
+                    level=logging.ERROR,
+                    filename=u'{0}/ViberBot.log'.format(os.getcwd()))
+
+
+def main():
+    try:
+        scheduler = sched.scheduler(time.time(), time.sleep)
+        scheduler.enter(5, 1, set_webhook, (viber,))
+        t = threading.Thread(target=scheduler.run)
+        t.start()
+        app.run(host='0.0.0.0', port=8000)
+    except Exception as e:
+        logging.error(e)
 
 if __name__ == "__main__":
-    scheduler = sched.scheduler(time.time(), time.sleep)
-    scheduler.enter(5, 1, set_webhook, (viber,))
-    t = threading.Thread(target=scheduler.run)
-    t.start()
-    app.run(host='0.0.0.0', port=8000)
+    main()
 
