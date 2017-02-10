7b07fe664fcf14ab710978b939bbf6b6d4c4db02
MaxOvcharov <ma12xxx@rambler.ru>
2017-02-10 09:30:35 +0300

    Added new settings to init.sh

--- init.sh
+++ init.sh
@@ -3,17 +3,18 @@
 # NGINX settings
 sudo rm -rf /etc/nginx/sites-enabled/*
 sudo chmod 777 nginx.conf
-sudo cp nginx.conf /etc/nginx/sites-enabled/nginx.conf
+sudo cp -v nginx.conf /etc/nginx/sites-enabled/nginx.conf
 sudo nginx -t
 sudo /etc/init.d/nginx restart
 
 # Supervisor settings
 sudo unlink /var/run/supervisor.sock
-sudo cp gunicorn.conf /etc/supervisor/conf.d/
-sudo supervisord -c /etc/supervisor/supervisord.conf
+sudo cp -v gunicorn.conf /etc/supervisor/conf.d/
+sudo alias supervisorctl="supervisorctl -c /etc/supervisor/supervisord.conf"
 
 # Create a systemd Unit File
-sudo cp ViberBot.service /etc/systemd/system/
+sudo cp -v ViberBot.service /etc/systemd/system/
+sudo systemctl enable ViberBot.service
 sudo systemctl start ViberBot.service
 sudo systemctl daemon-reload
 
