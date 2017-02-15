dc1375aa2cf9df0300b12d95308e3d62ca3f93c7
MaxOvcharov <ovcharovmax@yandex.ru>
2017-02-15 16:31:20 +0300

    Added migration manager

--- ViberBot/manage.py
+++ ViberBot/manage.py
@@ -1,15 +1,8 @@
 from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
-from database import db
-from simpleViberBot.simpleViberBot import app
+from . import create_app
+from flask_migrate import MigrateCommand
 
 
-app.config.from_object('ViberBot.config.config')
-migrate = Migrate(app, db)
-manager = Manager(app)
-
+manager = Manager(create_app)
+manager.add_option("-c", "--config", dest="config_module", required=False)
 manager.add_command('db', MigrateCommand)
-
-
-if __name__ == '__main__':
-    manager.run()
