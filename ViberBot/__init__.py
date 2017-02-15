94f58b7ae1bd16f8e1abbf750a01246e21ad64fe
MaxOvcharov <ovcharovmax@yandex.ru>
2017-02-15 12:28:53 +0000

    Bug fixed: best way of initialize context_app

--- ViberBot/__init__.py
+++ ViberBot/__init__.py
@@ -8,4 +8,8 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object('ViberBot.config.config')
     db.init_app(app)
+    with app.app_context():
+        # Extensions like Flask-SQLAlchemy now know what the "current" app
+        # is while within this block. Therefore, you can now run........
+        db.create_all()
     return app
